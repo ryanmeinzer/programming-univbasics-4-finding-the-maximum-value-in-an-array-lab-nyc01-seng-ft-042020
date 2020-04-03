21e3622e71a763962d64b6c3673e00d6079aade7
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-03 18:11:02 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,10 +1,11 @@
 def find_max_value(array)
   count = 0
+  max_value = -1
   while count < array.length do
-    if array[count] >= array[count+1]
-    find_max_value
+    if max_value < array[count]
+    max_value = array[count]
     end
     count += 1
   end
-    return count
+    max_value
 end
